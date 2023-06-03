515d202e4f30643684ee8dba3f87d4ebbb527830
Johan Romeo <jofiluren@outlook.com>
2023-06-03 09:39:57 +0200

    Lade till main klass

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var rectangle = new Rectangle();
+        // w or h > 1023 is too large
+        System.out.println(rectangle.calculateArea(1023, 1023));
+    }
+}
